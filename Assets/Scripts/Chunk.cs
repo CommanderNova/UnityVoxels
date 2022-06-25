ceceb8be5cb4e52640570ae87440792673c3dca0
CommanderNova <marklazarevski44@gmail.com>
2022-06-25 20:05:40 +0200

    Made the chunk generate more than one block at the correct position

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -15,6 +15,8 @@ public class Chunk : MonoBehaviour
     [SerializeField]
     private int height = 1;
 
+    private int MaxBlockCount => width * depth * height;
+    
     private Mesh mesh;
     private Vector3[] vertices;
     private int[] triangles;
@@ -25,62 +27,152 @@ public class Chunk : MonoBehaviour
         GetComponent<MeshFilter>().mesh = mesh;
 
         var startTime = Time.realtimeSinceStartupAsDouble;
+
+        vertices = new Vector3[MaxBlockCount * 8];
+        var verticesIndex = -1;
+
+        triangles = new int[MaxBlockCount * 36];
+        var triangleIndex = -1;
+
         for (var x = 0; x < width; ++x)
         {
             for (var z = 0; z < depth; ++z)
             {
                 for (var y = 0; y < height; ++y)
                 {
-                    // [B = Back; F = Forward] [B = Bottom, T = Top] [L = Left, R = Right]
-                    vertices = new[]
-                    {
-                        // Bottom
-                        new Vector3(0, 0, 0), // BBL 0
-                        new Vector3(0, 0, 1), // FBL 1
-                        new Vector3(1, 0, 1), // FBR 2
-                        new Vector3(1, 0, 0), // BBR 3
-                        
-                        // Top
-                        new Vector3(0, 1, 0), // BTL 4
-                        new Vector3(1, 1, 0), // BTR 5
-                        new Vector3(0, 1, 1), // FTL 6
-                        new Vector3(1, 1, 1), // FTR 7
-                    };
+                    var blockType = GetBlock(x, y, z);
 
-                    triangles = new[]
+                    if (blockType != BlockTypes.Solid)
                     {
-                        // Bottom Face
-                        0, 2, 1,
-                        0, 3, 2,
-                        
-                        // Back Face
-                        0, 4, 3,
-                        4, 5, 3,
-                        
-                        // Front Face
-                        1, 2, 6,
-                        6, 2, 7,
-                        
-                        // Top Face
-                        4, 6, 5,
-                        5, 6, 7, 
-                        
-                        // Left Face
-                        0, 1, 6,
-                        0, 6, 4,
-                        
-                        // Right Face
-                        3, 5, 2,
-                        2, 5, 7,
-                    };
+                        continue;
+                    }
                     
-                    mesh.Clear();
+                    // [B = Back; F = Forward] [B = Bottom, T = Top] [L = Left, R = Right]
+                    
+                    verticesIndex++;
+                    vertices[verticesIndex] = new Vector3(0 + x, 0 + y, 0 + z); // BBL 0
+                    
+                    verticesIndex++;
+                    vertices[verticesIndex] = new Vector3(0 + x, 0 + y, 1 + z); // FBL 1
+                    
+                    verticesIndex++;
+                    vertices[verticesIndex] = new Vector3(1 + x, 0 + y, 1 + z); // FBR 2
+                    
+                    verticesIndex++;
+                    vertices[verticesIndex] = new Vector3(1 + x, 0 + y, 0 + z); // BBR 3
+                    
+                    verticesIndex++;
+                    vertices[verticesIndex] = new Vector3(0 + x, 1 + y, 0 + z); // BTL 4
+                    
+                    verticesIndex++;
+                    vertices[verticesIndex] = new Vector3(1 + x, 1 + y, 0 + z); // BTR 5
+                    
+                    verticesIndex++;
+                    vertices[verticesIndex] = new Vector3(0 + x, 1 + y, 1 + z); // FTL 6
+                    
+                    verticesIndex++;
+                    vertices[verticesIndex] = new Vector3(1 + x, 1 + y, 1 + z); // FTR 7
 
-                    mesh.vertices = vertices;
-                    mesh.triangles = triangles;
+                    // Start index for the current block of vertices
+                    var verticesStartIndex = verticesIndex - 7;
+                    
+                    // Bottom Face
+                    triangleIndex++;
+                    triangles[triangleIndex] = verticesStartIndex + 0;
+                    triangleIndex++;
+                    triangles[triangleIndex] = verticesStartIndex + 2;
+                    triangleIndex++;
+                    triangles[triangleIndex] = verticesStartIndex + 1;
+                    
+                    triangleIndex++;
+                    triangles[triangleIndex] = verticesStartIndex + 0;
+                    triangleIndex++;
+                    triangles[triangleIndex] = verticesStartIndex + 3;
+                    triangleIndex++;
+                    triangles[triangleIndex] = verticesStartIndex + 2;
+                    
+                    // Back Face
+                    triangleIndex++;
+                    triangles[triangleIndex] = verticesStartIndex + 0;
+                    triangleIndex++;
+                    triangles[triangleIndex] = verticesStartIndex + 4;
+                    triangleIndex++;
+                    triangles[triangleIndex] = verticesStartIndex + 3;
+                    
+                    triangleIndex++;
+                    triangles[triangleIndex] = verticesStartIndex + 4;
+                    triangleIndex++;
+                    triangles[triangleIndex] = verticesStartIndex + 5;
+                    triangleIndex++;
+                    triangles[triangleIndex] = verticesStartIndex + 3;
+                    
+                    // Front Face
+                    triangleIndex++;
+                    triangles[triangleIndex] = verticesStartIndex + 1;
+                    triangleIndex++;
+                    triangles[triangleIndex] = verticesStartIndex + 2;
+                    triangleIndex++;
+                    triangles[triangleIndex] = verticesStartIndex + 6;
+                    
+                    triangleIndex++;
+                    triangles[triangleIndex] = verticesStartIndex + 6;
+                    triangleIndex++;
+                    triangles[triangleIndex] = verticesStartIndex + 2;
+                    triangleIndex++;
+                    triangles[triangleIndex] = verticesStartIndex + 7;
+                    
+                    // Top Face
+                    triangleIndex++;
+                    triangles[triangleIndex] = verticesStartIndex + 4;
+                    triangleIndex++;
+                    triangles[triangleIndex] = verticesStartIndex + 6;
+                    triangleIndex++;
+                    triangles[triangleIndex] = verticesStartIndex + 5;
+                    
+                    triangleIndex++;
+                    triangles[triangleIndex] = verticesStartIndex + 5;
+                    triangleIndex++;
+                    triangles[triangleIndex] = verticesStartIndex + 6;
+                    triangleIndex++;
+                    triangles[triangleIndex] = verticesStartIndex + 7;
+                    
+                    // Left Face
+                    triangleIndex++;
+                    triangles[triangleIndex] = verticesStartIndex + 0;
+                    triangleIndex++;
+                    triangles[triangleIndex] = verticesStartIndex + 1;
+                    triangleIndex++;
+                    triangles[triangleIndex] = verticesStartIndex + 6;
+                    
+                    triangleIndex++;
+                    triangles[triangleIndex] = verticesStartIndex + 0;
+                    triangleIndex++;
+                    triangles[triangleIndex] = verticesStartIndex + 6;
+                    triangleIndex++;
+                    triangles[triangleIndex] = verticesStartIndex + 4;
+                    
+                    // Right Face
+                    triangleIndex++;
+                    triangles[triangleIndex] = verticesStartIndex + 3;
+                    triangleIndex++;
+                    triangles[triangleIndex] = verticesStartIndex + 5;
+                    triangleIndex++;
+                    triangles[triangleIndex] = verticesStartIndex + 2;
+                    
+                    triangleIndex++;
+                    triangles[triangleIndex] = verticesStartIndex + 2;
+                    triangleIndex++;
+                    triangles[triangleIndex] = verticesStartIndex + 5;
+                    triangleIndex++;
+                    triangles[triangleIndex] = verticesStartIndex + 7;
                 }
             }
         }
+        
+        mesh.Clear();
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
 
         var duration = Time.realtimeSinceStartupAsDouble - startTime;
         print("Duration: " + duration);
